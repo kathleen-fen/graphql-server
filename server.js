55892766313d9cce47830c08c408d0d63050b6e1
Kathreen Fenchenko <fev3@mail.ru>
2023-07-10 08:46:54 +0200

    simple graph ql request

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+
 const app = express();
+const schema = buildSchema(`
+type Query {
+  hello: String
+}
+`);
+const root = {
+  hello: () => {
+    return "Hello world!";
+  },
+};
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 app.listen(5000, () => console.log("Server is running!"));
